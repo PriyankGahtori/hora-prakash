4eeb4ab9dc13a8db5a9730c17437d7fedb7a212e
Priyank Gahtori <priyankgahtori@Priyanks-MacBook-Pro.local>
2026-04-18 07:49:38 +0530

    feat: scaffold Vite project with tab shell

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// src/main.js
+import { initTabs } from './ui/tabs.js'
+import { renderInputTab } from './tabs/input.js'
+
+initTabs()
+renderInputTab()
---
+++ src/state.js
@@ -0,0 +1,9 @@
+// src/state.js
+export const state = {
+  birth: null,       // { name, dob, tob, lat, lon, timezone, location }
+  planets: null,     // array of { id, name, lon, house, nakshatra, pada, retrograde }
+  lagna: null,       // { lon, house: 1, nakshatra, pada }
+  houses: null,      // array of 12 house cusp longitudes
+  dasha: null,       // computed dasha tree
+  panchang: null,    // computed panchang values
+}
